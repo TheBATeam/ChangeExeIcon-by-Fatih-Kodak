9169d1d84c2276b4ebbb05d8d9e30d4d41ff04c9
Ankitamehra93 <48797537+Ankitamehra93@users.noreply.github.com>
2020-07-01 23:29:30 +0530

    First Commit

---
+++ Source Code/Test.bat
@@ -0,0 +1,13 @@
+@Echo off
+cls
+
+Title ChangeExeIcon - Demo - www.thebateam.org
+Cd Files
+
+ChangeExeIcon BT-Logo.ico ChangeExeIcon.exe ..\ChangeExeIcon_with_LOGO.exe
+Echo.
+Echo. TheBATeam's Icon must be added to the Plugin itself...
+Echo. it is generated in the Same Folder...
+Echo.
+Pause
+exit
